d024316b639727a498adf774550f385a98f3b0df
David Li <david.li3100@gmail.com>
2020-09-11 10:49:39 -0400

    chore: add driver code for binary search

--- examples/searching/Binary Search/index.js
+++ examples/searching/Binary Search/index.js
@@ -1,26 +1,25 @@
 /**
  * Binary Search
- * 
+ *
  * Given an element X in an array, binary search will find whether
  * or not X exists in that array
- * 
+ *
  * NOTE: this assumes the array is sorted
- * 
+ *
  * This is significantly faster than Linear Search [O(N) time complexity]
  * with a time complexity of [O(logN)]
  *
  */
 
- /**
-  * Recursively searches the array for the element x
-  * @param {array} array sorted array to search in 
-  * @param {any} x the element to find
-  * @param {number} start starting index of search
-  * @param {number} end ending index of search
-  * @returns {boolean} does the element x exist in the array 
-  */
- const recursiveSearch = (array, x, start, end) => {
-
+/**
+ * Recursively searches the array for the element x
+ * @param {array} array sorted array to search in
+ * @param {any} x the element to find
+ * @param {number} start starting index of search
+ * @param {number} end ending index of search
+ * @returns {boolean} does the element x exist in the array
+ */
+const recursiveSearch = (array, x, start, end) => {
   /**
    * Base condition
    * if the starting index is greater than the ending index
@@ -32,16 +31,16 @@
   /**
    * Binary search works by essentially splitting the sorted
    * array in half to find a value, we split the array in half here
-   * 
+   *
    * To find the middle index of the array, we add up the starting and ending
    * index, divide that by 2, and round down
    */
-  let middleOfArray = Math.floor((start + end)/2);
+  let middleOfArray = Math.floor((start + end) / 2);
 
   /**
    * Before we do anything else, let's see if the
    * middle item in the array matches the one we're searching for
-   * 
+   *
    * NOTE: Javascript uses triple equality - if you're interested in why,
    * google to learn more about truthy/falsy in Javascript
    */
@@ -50,21 +49,28 @@
   /**
    * Now for some actual searching
    * There are two possible cases here:
-   * 
+   *
    *    1. The element in the middle is greater than `X`
    *       - search on the left side of the array since all those values are
-   *         guaranteed to be less than `array[middleOfArray]`  
+   *         guaranteed to be less than `array[middleOfArray]`
    *    2. The element in the middle is less than `X`
    *       - search on the right side of the array since all those values are
    *         guaranteed to be greater than `array[middleOfArray]`
    */
 
-   if (array[middleOfArray] > x) {
-      // we recursively call the binary search function with a new domain to search
-      return recursiveSearch(array, x, start, mid-1);
-   } else {
-     // we recursively call the binary search function with a new domain to search
-     return recursiveSearch(array, x, mid+1, end);
-   }
-}
+  if (array[middleOfArray] > x) {
+    // we recursively call the binary search function with a new domain to search
+    return recursiveSearch(array, x, start, middleOfArray - 1);
+  } else {
+    // we recursively call the binary search function with a new domain to search
+    return recursiveSearch(array, x, middleOfArray + 1, end);
+  }
+};
 
+// driver code for testing
+let array = [1, 3, 5, 8, 10];
+let x = 8;
+
+recursiveSearch(array, x, 0, array.length - 1)
+  ? console.log(`${x} was found in the array`)
+  : console.error(` ${x} was not found in the array`);
