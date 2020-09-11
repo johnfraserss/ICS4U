2e6e9ed2ccea859a4415c3964ab877e171db84e3
David Li <david.li3100@gmail.com>
2020-09-11 10:37:42 -0400

    feat: linear search boilerplate

---
+++ examples/searching/Linear Search/index.js
@@ -0,0 +1,23 @@
+/**
+ * Binary Search
+ * 
+ * Given an element X in an array, binary search will find whether
+ * or not X exists in that array
+ * 
+ * NOTE: this assumes the array is sorted
+ * 
+ * This is significantly faster than Linear Search [O(N) time complexity]
+ * with a time complexity of [O(logN)]
+ *
+ */
+
+ /**
+  * Recursively searches the array for the element x
+  * @param {array} array 
+  * @param {any} x 
+  * @param {number} start 
+  * @param {number} end 
+  */
+ const recursiveSearch = (array, x, start, end) {
+
+ }
