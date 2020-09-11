97ea018ddae718d7bbcfcaa6d4d7f7e5d354f9ba
David Li <david.li3100@gmail.com>
2020-09-11 15:21:04 -0400

    add insertion sort

---
+++ examples/sorting/Insertion Sort/index.js
@@ -0,0 +1,37 @@
+/**
+ * Insert sort
+ * 
+ * Time complexity: O(N^2)
+ *   - outperforms more complex sorts with small lists due to its low hidden constant value
+ * 
+ * Insertion sort is an in-place sort that performs comparisons to sort an array.
+ * It starts at a given item in the list, and then looks to move it to the left so that 
+ * the sublist to the left of the chosen index is sorted in ascending order.
+ * 
+ * This means that by the time the sort moves the last item in the array, the array
+ * would already be sorted
+ */
+
+const insertionSort = (array) => {
+  let length = array.length;
+  for (let i = 1; i < length; i++) {
+    // use the current array item as a key to compare
+    let key = array[i];
+
+    // set index to start comparison with
+    let j = i - 1;
+    // find a spot in the left sublist for the key to go
+    while (j >= 0 && array[j] > key) {
+        array[j + 1] = array[j];
+        j = j - 1;
+    }
+    // reset the values
+    array[j + 1] = key;
+  }
+  return array;
+};
+ 
+
+// driver code to test
+const array = [2, 1, 3, 4, 5];
+console.log(insertionSort(array)); // should equal [1, 2, 3, 4, 5]
