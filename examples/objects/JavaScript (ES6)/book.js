b12f3ee88b475b5da1866bef179da4e9ff7bd2d2
David Li <david.li3100@gmail.com>
2020-10-06 10:25:30 -0400

    feat (es6): add import/export with commonJS

--- examples/objects/JavaScript (ES6)/book.js
+++ examples/objects/JavaScript (ES6)/book.js
@@ -62,26 +62,7 @@ class Book {
     this.price = this.price + value;
     return true;
   }
-
 }
 
-/**
- * Function used to test the creation of objects
- */
-function testing(){
-    var bookOne = new Book("Terry Pratchett", "Guards! Guards!", 5.99);
-    var bookTwo = new Book("Robert Jordan", "The Eye of the World", 8.99);
-    console.log(bookOne.getAuthor());
-    console.log(bookOne.getPrice());
-    console.log(bookOne.getTitle());
-    bookOne.increasePrice(1.00);
-    console.log(bookOne.getPrice());
-
-    console.log(bookTwo.getAuthor());
-    console.log(bookTwo.getPrice());
-    console.log(bookTwo.getTitle());
-    bookTwo.increasePrice(-6.00);
-    console.log(bookTwo.getPrice());
-}
-
-testing();
+// export the book class
+module.exports = Book;
