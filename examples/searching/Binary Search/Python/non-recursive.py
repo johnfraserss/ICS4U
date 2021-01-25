cf13fa11337d1010a7004eddd399070b3ba8f24a
Multivalence <47169252+Multivalence@users.noreply.github.com>
2021-01-25 12:38:59 -0500

    Update non_recursive.py to conform to PEP-8
    Updating non_recursive.py indentation to conform to PEP-8 style guidelines

--- examples/searching/Binary Search/Python/non-recursive.py
+++ examples/searching/Binary Search/Python/non-recursive.py
@@ -13,56 +13,58 @@
 import math
 
 def nonRecursiveSearch(array, x):
-  '''
-  Recursively searches the array for the element x
+    '''
+    Recursively searches the array for the element x
   
-      Parameters:
+    Parameters:
         array (array): sorted array to search in
         x (any): the element to find
 
-      Returns:
+    Returns:
         boolean: does the element x exist in the array
-  '''
+    '''
   
-  start = 0
-  end = len(array) - 1
-
-  '''
-  Instead of recursing, we use a while loop to perform the multiplication
-  While the starting index of the search is less than or equal to the ending index,
-  we perform binary search
-  '''
-  while (start <= end):
+    start = 0
+    end = len(array) - 1
 
     '''
-    Binary search works by essentially splitting the sorted
-    array in half to find a value, we split the array in half here
+    Instead of recursing, we use a while loop to perform the multiplication
+    While the starting index of the search is less than or equal to the ending index,
+    we perform binary search
+    '''
+    while (start <= end):
+
+        '''
+        Binary search works by essentially splitting the sorted
+        array in half to find a value, we split the array in half here
     
-    To find the middle index of the array, we add up the starting and ending
-    index, divide that by 2, and round down
-    '''
-    middleOfArray = math.floor((start + end) / 2)
+        To find the middle index of the array, we add up the starting and ending
+        index, divide that by 2, and round down
+        '''
+        middleOfArray = math.floor((start + end) / 2)
 
-    '''
-    Before we do anything else, let's see if the
-    middle item in the array matches the one we're searching for
-    '''
-    if (array[middleOfArray] == x): 
-      return True
+        '''
+        Before we do anything else, let's see if the
+        middle item in the array matches the one we're searching for
+        '''
+        if (array[middleOfArray] == x): 
+            return True
     
-    # Now for some actual searching
-    # There are two possible cases here:
+        # Now for some actual searching
+        # There are two possible cases here:
 
-    #   1. The element in the middle is less than X
-    #     - search on the right side of the array since all those values are
-    #       guaranteed to be greater than array[middleOfArray]    
-    #   2. The element in the middle is greater than X
-    #     - search on the left side of the array since all those values are
-    #       guaranteed to be less than array[middleOfArray]
-    elif (array[middleOfArray] < x):
-      start = middleOfArray + 1
-    else:
-      end = middleOfArray - 1
+        #   1. The element in the middle is less than X
+        #     - search on the right side of the array since all those values are
+        #       guaranteed to be greater than array[middleOfArray]    
+        #   2. The element in the middle is greater than X
+        #     - search on the left side of the array since all those values are
+        #       guaranteed to be less than array[middleOfArray]
+        
+        elif (array[middleOfArray] < x):
+            start = middleOfArray + 1
+          
+        else:
+            end = middleOfArray - 1
 
 # driver code for testing
 array = [1, 3, 5, 8, 10]
