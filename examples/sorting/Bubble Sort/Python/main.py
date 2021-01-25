dfd775cba94fa895518b3e73f9dbb913cdbb9cc8
Multivalence <47169252+Multivalence@users.noreply.github.com>
2021-01-25 12:52:20 -0500

    Update main.py to conform to PEP-8
    Updating main.py indentation to conform to PEP-8 style guidelines

--- examples/sorting/Bubble Sort/Python/main.py
+++ examples/sorting/Bubble Sort/Python/main.py
@@ -9,35 +9,35 @@ array value with the one to the right of it.
 
 
 def bubbleSort(array):
-  '''
-  Sorts an array in ascending order
+    '''
+    Sorts an array in ascending order
 
-      Parameters:
+    Parameters:
         array (array): the array to sort
 
-      Returns:
+    Returns:
         (array): the sorted array
-  '''
-  length = len(array)
-  swapped = ''
+    '''
+    length = len(array)
+    swapped = ''
 
-  '''
-  Using the python equivalent of a do while loop to minimize the amount 
-  of bubbling needed to verify that an array is sorted
-  '''
-  while True:
-      swapped = False
-      # loop through the entire array to perform swaps
-      for i in range(0, length-1):
-        # if the current value is greater than the value to the right of it, swap
-        if (array[i] > array[i + 1]):
-            tempValue = array[i]
-            array[i] = array[i + 1];
-            array[i + 1] = tempValue;
-            swapped = True
-      if (swapped):
-        break
-  return array
+    '''
+    Using the python equivalent of a do while loop to minimize the amount 
+    of bubbling needed to verify that an array is sorted
+    '''
+    while True:
+        swapped = False
+        # loop through the entire array to perform swaps
+        for i in range(0, length-1):
+            # if the current value is greater than the value to the right of it, swap
+            if (array[i] > array[i + 1]):
+                tempValue = array[i]
+                array[i] = array[i + 1];
+                array[i + 1] = tempValue;
+                swapped = True
+        if(swapped):
+            break
+    return array
 
 # driver code to test
 array = [2, 1, 3, 4, 5]
