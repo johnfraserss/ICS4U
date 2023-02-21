4e11fc66d87427721003f312ab958df432c1c975
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-02-21 14:29:59 -0500

    fix: wrapper function for fib memo

--- examples/recursion/Go/fibonacci-memoization.go
+++ examples/recursion/Go/fibonacci-memoization.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-// The fibonacci sequence is 1, 1, and then the sum of the previous two elements
-func fibonacci(n int, memoizationTable map[int]int) int {
+// Optimized using memoization
+func fibonacci_memoize(n int, memoizationTable map[int]int) int {
 	// Base case: the 1st and 2nd value in the fibonacci sequence, which are 1
 	if n == 0 || n == 1 {
 		return 1
@@ -11,16 +11,21 @@ func fibonacci(n int, memoizationTable map[int]int) int {
 
 	// If the nth fibonacci doesn't exist in our memo table, generate it
 	if _, exists := memoizationTable[n]; !exists {
-		memoizationTable[n] = fibonacci(n-1, memoizationTable) + fibonacci(n-2, memoizationTable)
+		memoizationTable[n] = fibonacci_memoize(n-1, memoizationTable) + fibonacci_memoize(n-2, memoizationTable)
 	}
 
 	return memoizationTable[n]
 }
 
+// The fibonacci sequence is 1, 1, and then the sum of the previous two elements
+func fibonacci(n int) int {
+	memorizationTable := make(map[int]int)
+	return fibonacci_memoize(n, memorizationTable)
+}
+
 func main() {
 	// Basic driver code that gets the first 20 fibonacci numbers
 	for n := 0; n < 20; n++ {
-		memorizationTable := make(map[int]int)
-		fmt.Printf("Fibonacci # at idx %d: %d\n", n+1, fibonacci(n, memorizationTable))
+		fmt.Printf("Fibonacci # at idx %d: %d\n", n+1, fibonacci(n))
 	}
 }
