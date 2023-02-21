d6ecb8176e7b1e79feffaf2061daf3d3cbcea46e
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-02-21 12:52:54 -0500

    add better docs for objects

--- examples/objects/Go/book.go
+++ examples/objects/Go/book.go
@@ -1,15 +1,42 @@
+package main
+
 // A struct representing a book, storing information about it
 // Note that Go doesn't actually have classes (it's not really OOP https://go.dev/doc/faq#Is_Go_an_object-oriented_language),
 // hence why structs are used instead.
-package main
-
 type Book struct {
 	Author, Title string
 	Price         float64
 }
 
-// IncreasePrice attempts to increase the price of a book.
-// Returns whether it was successful in modifying the price.
+// Constructor function to create a new book.
+//
+// Parameters:
+//
+//	author, string:
+//	  the author of the book
+//	title, string:
+//	  the title of the book
+//	price, float64:
+//	  the decimal price of the book
+//
+// Returns a pointer to new Book object
+func NewBook(author string, title string, price float64) *Book {
+	book := new(Book)
+	book.Author = author
+	book.Title = title
+	book.Price = price
+
+	return book
+}
+
+// Method on the struct Book that attempts to increase the price of a book.
+//
+// Parameters:
+//
+//	increase, float64:
+//	   the decimal value to try and increase the price of the book by
+//
+// Returns whether it was successful in modifying the price
 func (book *Book) IncreasePrice(increase float64) bool {
 	if increase < 0 {
 		// This is a decrease, not an increase
