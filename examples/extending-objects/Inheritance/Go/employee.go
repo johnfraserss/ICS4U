3e224e20fde3617ed17e275c434ad27f430daaa8
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-02-21 13:00:50 -0500

    docs: better comments for inheritance

--- examples/extending-objects/Inheritance/Go/employee.go
+++ examples/extending-objects/Inheritance/Go/employee.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+// Inherits a Person object, and adds on a staffNumber
 type Employee struct {
 	Person
 	staffNumber uint
 }
 
+// Returns a string representation of the employee
 func (employee Employee) String() string {
 	return fmt.Sprintf("%s, %d", employee.Person, employee.staffNumber)
 }
--- examples/extending-objects/Inheritance/Go/main.go
+++ examples/extending-objects/Inheritance/Go/main.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	// Create an object representing Marge (Person)
 	marge := Person{
 		firstName: "Marge",
 		lastName:  "Simpson",
 		age:       36,
 	}
 
+	// Create an object representing Homer (Employee)
 	homer := Employee{
+		// Specifying details for parent class
 		Person: Person{
 			firstName: "Homer",
 			lastName:  "Simpson",
