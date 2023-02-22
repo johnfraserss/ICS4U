3855ab1ba1a5c587cdb355d1554a0233794828f2
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-02-22 11:44:15 -0500

    update searching algos to return index (#37)

--- examples/searching/Binary Search/Go/recursive.go
+++ examples/searching/Binary Search/Go/recursive.go
@@ -1,13 +1,13 @@
 /**
-  * Binary search
-  *
-  * Time complexity: O(logN) - significantly faster than linear search at scale
-  *
-  * Given an element X in an array, binary search will find whether or not X
-  * exists in that array.
-  *
-  * NOTE: this assumes the array is sorted
-*/
+ * Binary search
+ *
+ * Time complexity: O(logN) - significantly faster than linear search at scale
+ *
+ * Given an element X in an array, binary search will find whether or not X
+ * exists in that array.
+ *
+ * NOTE: this assumes the array is sorted
+ */
 
 package main
 
@@ -15,62 +15,63 @@ import "fmt"
 
 // Recursively search sorted array arr for element x.
 // Returns whether element x is within array arr or not.
-func recursiveSearch(arr []int, x int) bool {
-    // Base condition if the slice from a prev iter is empty. Empty -> no result.
-    if len(arr) == 0 {
-        return false
-    }
+func recursiveSearch(arr []int, x int) int {
+	// Base condition if the slice from a prev iter is empty. Empty -> no result.
+	if len(arr) == 0 {
+		return -1
+	}
 
-    // Binary search works by essentially splitting the sorted array in half to
-    // find a value. The middle point is calculated here.
-    //
-    // To find the middle index of the array, we add up the starting and ending
-    // index, divide by 2, and round down (auto due to int division)
-    start, end := 0, len(arr)
-    middle := (start + end) / 2
+	// Binary search works by essentially splitting the sorted array in half to
+	// find a value. The middle point is calculated here.
+	//
+	// To find the middle index of the array, we add up the starting and ending
+	// index, divide by 2, and round down (auto due to int division)
+	start, end := 0, len(arr)
+	middle := (start + end) / 2
 
-    // There are three possible cases.
+	// There are three possible cases.
 
-    if arr[middle] == x {
-        // 1. The middle element matches element X. 
-        return true
-    } else if arr[middle] < x {
-        // 2. The middle element is **less** than X - search the right side
-        // of the array since all those values are guaranteed to be greater
-        // than the middle element.
-        start = middle + 1
+	if arr[middle] == x {
+		// 1. The middle element matches element X.
+		return x
+	} else if arr[middle] < x {
+		// 2. The middle element is **less** than X - search the right side
+		// of the array since all those values are guaranteed to be greater
+		// than the middle element.
+		start = middle + 1
 
-        // No such array can exist, so element x is not in array
-        if start > end {
-            return false
-        }
+		// No such array can exist, so element x is not in array
+		if start > end {
+			return -1
+		}
 
-        // Recursively call function with new domain to search
-        return recursiveSearch(arr[start:end], x)
-    } else {
-        // 3. The middle element is **more** than X - search the left side
-        // of the array since all those values are guaranteed to be less
-        // than the middle element.
-        end = middle - 1
+		// Recursively call function with new domain to search
+		return recursiveSearch(arr[start:end], x)
+	} else {
+		// 3. The middle element is **more** than X - search the left side
+		// of the array since all those values are guaranteed to be less
+		// than the middle element.
+		end = middle - 1
 
-        // No such array can exist, so element x is not in array
-	if start > end {
-             return false
-        }
+		// No such array can exist, so element x is not in array
+		if start > end {
+			return -1
+		}
 
-        // Recursively call function with new domain to search, adding 1
-	// to the end index to ensure it's included in the slice
-        return recursiveSearch(arr[start:end+1], x)
-    }
+		// Recursively call function with new domain to search, adding 1
+		// to the end index to ensure it's included in the slice
+		return recursiveSearch(arr[start:end+1], x)
+	}
 }
 
 func main() {
-    arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-    x := 9
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	x := 9
+	pos := recursiveSearch(arr, x)
 
-    if recursiveSearch(arr, x) {
-        fmt.Printf("%v was found in the array.\n", x)
-    } else {
-        fmt.Printf("%v was not found in the array.\n", x)
-    }
+	if pos != -1 {
+		fmt.Printf("%v was found in the array at pos %d.\n", x, pos)
+	} else {
+		fmt.Printf("%v was not found in the array.\n", x)
+	}
 }
--- examples/searching/Linear Search/Go/main.go
+++ examples/searching/Linear Search/Go/main.go
@@ -1,14 +1,14 @@
 /**
  * Linear search (a.k.a. the easier one)
- * 
+ *
  * Time complexity: O(N)
  *   - slower at large scales
- * 
+ *
  * Linear search is pretty self-descriptive.
- * Given an array, and an element X, loop through the entire 
- * array in order until you find X. 
- * 
- * The benefit of linear search is that the array doesn't have to 
+ * Given an array, and an element X, loop through the entire
+ * array in order until you find X.
+ *
+ * The benefit of linear search is that the array doesn't have to
  * be sorted since you're looping through the entire array.
  */
 
@@ -17,31 +17,29 @@ package main
 import "fmt"
 
 // linearSearch returns whether element x is in array arr.
-func linearSearch(arr []int, x int) bool {
-    // set found to false by default, if nothing matches, found will be unchanged and returned
-    found := false;
+func linearSearch(arr []int, x int) int {
+	// Using range, each element of the array is stored as "item" and compared to x.
+	// If it equals x, we set found to true and exit the loop.
+	// If none of them equal x, found remains false.
+	for i, item := range arr {
+		if item == x {
+			// Return position of the item in the array
+			return i
+		}
+	}
 
-    // Using range, each element of the array is stored as "item" and compared to x.
-    // If it equals x, we set found to true and exit the loop.
-    // If none of them equal x, found remains false.
-    for item := range arr {
-        if item == x {
-            found = true
-            break
-        }
-    }
-
-    return found
+	// Could not find item
+	return -1
 }
 
 func main() {
-    arr := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-    x := 5
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	x := 5
+	pos := linearSearch(arr, x)
 
-    if linearSearch(arr, x) {
-        fmt.Printf("%v was found in the array\n", x)
-    } else {
-        fmt.Printf("%v was not found in the array\n", x)
-    }
+	if pos != -1 {
+		fmt.Printf("%v was found in the array at pos %d.\n", x, pos)
+	} else {
+		fmt.Printf("%v was not found in the array\n", x)
+	}
 }
-
